4dd31ba4700afef516262a16fbf9e89e3d83a637
Estuardo Ureta <32362738+estuardo8u14@users.noreply.github.com>
2019-03-29 23:31:06 -0600

    Tests

---
+++ TestOp.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author Estuardo
+ *
+ */
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestOp {
+	
+	    
+	   
+	    
+	    @BeforeClass
+	    public static void setUpClass() {
+	    }
+	    
+	    @AfterClass
+	    public static void tearDownClass() {
+	    }
+
+	    /**
+	     * Test of Ordenar method, of class Operaciones.
+	     */
+	    @Test
+	    public void testOrdenar() {
+	        System.out.println("Ordenar");
+	        Paciente[] paciente = null;
+	        Operaciones instance = new Operaciones();
+	        instance.Ordenar(paciente);
+	        // TODO review the generated test code and remove the default call to fail.
+	        fail("The test case is a prototype.");
+	    }
+}
+	    
+	
